250096e7c5afa04f4cf431d02851126490dd15a0
Kevan Atkins <kevan.atkins@gmail.com>
2016-03-14 01:45:06 +1100

    Making a start on the basic layout, styling and feel.

---
+++ assets/js/src/zcustom.js
@@ -0,0 +1,10 @@
+var s = skrollr.init();
+skrollr.menu.init(s, {
+    animate: true,
+    easing: 'swing',
+    scale: 2,
+    duration: function(currentTop, targetTop) {
+        return 1500;
+    },
+    complexLinks: false
+});
