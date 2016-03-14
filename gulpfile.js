d4eb53d4751c7f062a0b53253c12009f96321ceb
Kevan Atkins <kevan.atkins@gmail.com>
2016-03-14 13:40:36 +1100

    Bypassing postcss for now, fixing type transforms for section headers and adjusting gradient for nav bar.

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('autoprefixer', function () {
     var sourcemaps   = require('gulp-sourcemaps');
     var autoprefixer = require('autoprefixer');
 
-    return gulp.src('_site/assets/css/main.css')
+    return gulp.src('_site/assets/css/src/main.css')
         .pipe(sourcemaps.init())
         .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
         .pipe(sourcemaps.write('.'))
@@ -28,7 +28,7 @@ gulp.task('autoprefixer', function () {
 
 gulp.task('watch', function () {
    gulp.watch(['assets/js/**/*.js', 'gulpfile.js'], ['js-fef']);
-   gulp.watch(['_site/assets/css/main.css'], ['autoprefixer']);
+   gulp.watch(['_site/assets/css/src/main.css'], ['autoprefixer']);
 });
 
 gulp.task('default', ['js-fef', 'autoprefixer'], function(){});
